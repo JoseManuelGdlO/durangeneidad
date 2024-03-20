d979c5d92c86a7d5e9dd6e3e1d4acb61f060b810
rjoelgs <rjoelgs@hotmail.com>
2024-03-20 17:38:51 -0600

    primer estructura login

--- src/app/modules/admin/pages/login/login.page.ts
+++ src/app/modules/admin/pages/login/login.page.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: 'login.page.html',
-    styleUrls: ['login.page.scss'],
-  })
-  export class LoginPage {
-    constructor() {}
+  selector: 'app-login-page',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
+})
+export class LoginPage {
+  hidePassword = true;
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
   }
+
+  get passwordIcon(): string {
+    return this.hidePassword ? '../../../../../assets/eye-closed-svgrepo-com.svg' : '../../../../../assets/eye-open-svgrepo-com.svg';
+  }
+}
