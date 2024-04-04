5d7f5245053b1b7ce784a72d0c963b0b43b51d2e
rjoelgs <rjoelgs@hotmail.com>
2024-04-03 22:31:05 -0600

    second post page

--- src/app/modules/admin/pages/post/post.page.ts
+++ src/app/modules/admin/pages/post/post.page.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import Quill from 'quill';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-post-page',
@@ -8,12 +9,47 @@ import Quill from 'quill';
 })
 export class PostPage implements AfterViewInit {
   @ViewChild('quillEditor') quillEditor!: ElementRef;
+  private quillInstance!: Quill;
+  creador: string = '';
+  creacion: string = ''; // Esta propiedad se llenará con la fecha actual en addPost()
+  titulo: string = '';
+  lugar: string = '';
+  tags: { label: string }[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngAfterViewInit(): void {
-    new Quill(this.quillEditor.nativeElement, {
+    this.quillInstance = new Quill(this.quillEditor.nativeElement, {
       theme: 'snow' // o 'bubble'
+    });
+  }
+
+  addPost(): void {
+    // Establece 'creacion' a la fecha actual en formato ISO antes de enviar
+    this.creacion = new Date().toISOString();
+
+    const articleBody = this.quillInstance.root.innerHTML; // Obtiene el contenido del editor Quill
+    const body = {
+      article: {
+        creador: this.creador,
+        creacion: this.creacion,
+        titulo: this.titulo,
+        body: articleBody,
+        lugar: "durango"
+      },
+      tags: this.tags
+    };
+
+    console.log(body)
+    this.http.post('http://3.218.160.237:8000/durangeneidad/add', body).subscribe({
+      next: (response) => {
+        console.log(response);
+        // Manejo de la respuesta exitosa
+      },
+      error: (error) => {
+        console.error(error);
+        // Manejo de errores
+      }
     });
   }
 }
